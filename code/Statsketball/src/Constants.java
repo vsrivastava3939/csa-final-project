deecb15da2502e89e02e19939678a0b93ac15b38
vsrivastava <vsrivastava3939@gmail.com>
2022-05-20 08:55:31 -0700

    moved stuff around

--- code/Statsketball/src/Constants.java
+++
@@ -1,9 +0,0 @@
-import java.awt.Font;
-
-public final class Constants {
-
-	public final static int WIDTH = 1200;
-	public final static int HEIGHT = 800;
-	public final static Font TITLE_FONT = new Font("Sanserif", 1, 40);
-	public final static Font LABEL_FONT = new Font("Sanserif", 0, 20);
-}
